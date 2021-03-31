f8c1cc6912abdcb2a22ef478210755d8b14a3735
vitoman8888 <vito.vittore@gmail.com>
2021-03-30 20:42:01 -0500

    Set up mutations and JWT logic

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,6 +12,11 @@ type User {
     friends: [User]
 }
 
+type Auth {
+    token: ID!
+    user: User
+}
+
 type Reaction {
     _id: ID
     username: String
@@ -29,11 +34,22 @@ type Thought {
 }
 
 type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
 }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+}
+
+
 `;
 
 
