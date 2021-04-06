f7f0930fbf156e701d7eae06326a853d0e72b93d
vitoman8888 <vito.vittore@gmail.com>
2021-04-05 19:05:08 -0500

    Oh, mLab is dead, hooking into Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const util = require('util');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_CONNECTION_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
